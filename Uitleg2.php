51bf2dbf1ad1650a2f73f156f0a9d01be8fcd302
Zelim Kubiev <zelimkubiev@live.nl>
2019-04-12 12:22:47 +0200

    Create Uitleg2.php

---
+++ Uitleg2.php
@@ -0,0 +1,61 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+</head>
+	
+<?php
+	
+	
+	Branch 2
+	
+	
+	class Animal {
+		private $name;
+		private $type;
+		private $sound;
+		
+		const AANTAL_WIELEN = 4;
+		
+		public function __construct($type = NULL){
+			self::addAnimal();
+			$this->type = $type;
+		}
+		
+		public static $aantalDieren = 100;
+		
+		public function setName($value){
+			$this->name = $value;
+		}
+		
+		public function getName(){
+			return $this->name;
+		}
+		
+		public static function addAnimal(){
+			self::$aantalDieren++;
+		}
+		
+		public function getType(){
+			return $this->type;
+		}
+	}
+//	
+//	Animal::addAnimal();
+//	
+//	
+	
+
+	
+	$aap = new Animal("Gorilla");
+	$aap->setName("Sander");
+	echo "<h1>Mijn dier is: {$aap->getType()}</h1>";
+	
+	echo Animal::$aantalDieren;
+	
+?>
+
+<body>
+</body>
+</html>
